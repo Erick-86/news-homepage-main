8c3346e0b91887b4eea1b680bc37377e1d080bf1
FreeMan <zelonaire@gmail.com>
2022-12-20 17:01:53 +0000

    creating functions for opening and closing mobile menu

--- main work/script.js
+++ main work/script.js
@@ -1,6 +1,16 @@
 const navMenu = document.getElementById("nav");
-const closeMenu = document.getElementById("closebtn")
+const closeMenuBtn = document.getElementById("closebtn")
 const openMenuBtn = document.getElementById("menubtn")
 // console.log(closeMenu,navMenu,openMenuBtn)
 
+//opening nav menu function
+openMenuBtn.addEventListener("click", openMenu)
+function openMenu() {
+        navMenu.classList.add("showMenu")
+}
 
+//closing nav menu function
+closeMenuBtn.addEventListener("click", closeMenu)
+function closeMenu() {
+        navMenu.classList.remove("showMenu")
+}
