802da64799e3c8266adf4c25d7aab504d55f859b
FreeMan <zelonaire@gmail.com>
2022-12-20 15:08:11 +0000

    getting acces to the nav menu, closing and oening btns

---
+++ main work/script.js
@@ -0,0 +1,6 @@
+const navMenu = document.getElementById("nav");
+const closeMenu = document.getElementById("closebtn")
+const openMenuBtn = document.getElementById("menubtn")
+// console.log(closeMenu,navMenu,openMenuBtn)
+
+
